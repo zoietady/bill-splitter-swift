3e97c608807d13be3a1f12bf2ef8a2b3b0ff4754
Zoie Tad-y <zoietady@gmail.com>
2020-07-13 19:59:14 +1000

    initial commit

--- BillSplit/ContentView.swift
+++ BillSplit/ContentView.swift
@@ -9,9 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State private var bill = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 4
+    
+    let tipPercentages = [0,5,10,15,20,25,30]
+    
+    var totalPerPerson: Double {
+        let amount = Double(bill) ?? 0
+        let splitBy = Double(numberOfPeople + 2)
+        let tipSelected = Double(tipPercentages[tipPercentage])
+        
+        let tipValue = amount / 100 * tipSelected
+        let grandTotal = amount + tipValue
+        let amountPerPerson = grandTotal / splitBy
+        
+        return amountPerPerson
     }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section(header:Text("How much is the total bill?")){
+                    TextField("Amount",text: $bill)
+                }
+                
+                Section(header:Text("Split between how many?")){
+                    Picker("Number of People", selection: $numberOfPeople){
+                        ForEach (2 ..< 100){
+                            Text("\($0) People")
+                        }
+                    }
+                }
+                
+                Section(header:Text("How much tip?")){
+                    Picker("Number of People", selection: $tipPercentage){
+                        ForEach (2 ..< tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header:Text("Amount per Person")){
+                    Text("$ \(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("Bill Split")
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
